32108589392e29b7b461008a7d01e1fa9c244c35
Daren-Ken St-laurent <daren511@hotmail.com>
2014-03-10 19:12:46 -0400

    seems really gud

--- Australie.java
+++ Australie.java
@@ -3,255 +3,242 @@ import javax.swing.*;
 
 public class Australie extends JApplet implements Runnable
 {
-	private Thread animation;
-	int tabCoordVilleX[] = new int[10];
-	int tabCoordVilleY[] = new int[10];
-	String tabNomVille[] = new String[10];
+   private Thread animation;
+   private final int NBVILLE = 10;
+   int tabCoordVilleX[] = new int[NBVILLE];
+   int tabCoordVilleY[] = new int[NBVILLE];
+   String tabNomVille[] = new String[NBVILLE];
    Polygon australie = new Polygon();
-   final int MAXX = this.size().width;
-   final int MAXY = this.size().height;
-   private int posX = (int)(Math.random()*MAXX);
-   private int posY = (int)(Math.random()*MAXY);
+   private int maxX = 0;
+   private int maxY = 0;
+   private int posX = 0;
+   private int posY = 0;
    private int direction = 0;
+   private final int GROSSEURHELICOPTERE = 15;
 
-	public void init()
-	{
-	  Boolean pasFini = true;
-	  int i= 0;
-	  int j= 0;
-	  String x = " ";
-	  String y = " ";
-	  int CoordX = 0;
-	  int CoordY = 0;
+   public void init()
+   {
+      maxX = this.size().width;
+      maxY = this.size().height;
+      posX = (int)(Math.random() * maxX);
+      posY = (int)(Math.random() * maxY);
 
-	  while(pasFini)
-	  {
-        x=getParameter("x"+i);
-        y=getParameter("y"+j);
-        if(x!=null && y!=null)
-        {
-         try
+      try
+      {
+         Boolean pasFini = true;
+         int i = 0;
+         int j = 0;
+         String x = " ";
+         String y = " ";
+         int CoordX = 0;
+         int CoordY = 0;
+
+         while (pasFini)
          {
-           CoordX = Integer.parseInt(x);
-           CoordY = Integer.parseInt(y);
-           australie.addPoint(CoordX,CoordY);
-           ++i;
-           ++j;
+            x = getParameter("x" + i);
+            y = getParameter("y" + j);
+            if (x != null && y != null)
+            {
+               try
+               {
+                  CoordX = Integer.parseInt(x);
+                  CoordY = Integer.parseInt(y);
+                  australie.addPoint(CoordX, CoordY);
+                  ++i;
+                  ++j;
+               }
+               catch (NumberFormatException e)
+               {
+                  pasFini = false;
+               }
+            }
+            else
+            {
+               pasFini = false;
+            }
          }
-         catch(NumberFormatException e)
+         for (int v = 0; v < tabNomVille.length; ++v) // boucle nom de ville
          {
-            pasFini = false;
+            if (getParameter("nomville" + v) != null)
+            {
+               tabNomVille[v] = getParameter("nomVille" + v);
+            }
          }
+         for (int b = 0; b < tabCoordVilleX.length; ++b) // boucle CoordX des villes
+         {
+            String TempVille = " ";
+            try
+            {
+               if (getParameter("xVille" + b) != (null))
+               {
+                  TempVille = getParameter("xVille" + b);
+                  tabCoordVilleX[b] = Integer.parseInt(TempVille);
+               }
+            }
+            catch (Exception e)
+            {
+               e.printStackTrace();
+            }
+         }
+         for (int n = 0; n < tabCoordVilleY.length; ++n) // boucle CoordY des villes
+         {
+            String TempVille = " ";
+            try
+            {
+               if (getParameter("yVille" + n) != (null))
+               {
+                  TempVille = getParameter("yVille" + n);
+                  tabCoordVilleY[n] = Integer.parseInt(TempVille);
+               }
+            }
+            catch (Exception e)
+            {
+               e.printStackTrace();
+            }
+         }
+      }
+      catch (Exception e) {}
+   }
 
-        }
-        else
-        {
-         pasFini = false;
-        }
+   public void start()
+   {
+      if (animation == null)
+      {
+         animation = new Thread(this);
+         animation.start();
+      }
+   }
 
-	  }
+   public void stop()
+   {
+      if (animation != null)
+      {
+         animation = null;
+      }
+   }
 
+   public void paint(Graphics g)
+   {
+      // affichage des objets graphiques
 
-		for(int v = 0 ; v<tabNomVille.length;++v) // boucle nom de ville
-		{
-			if (getParameter("nomville" + v)!=null)
-			{
-				tabNomVille[v] = getParameter("nomVille"+v);
-			}
-		}
-
-		for(int b = 0 ; b<tabCoordVilleX.length;++b) // boucle CoordX des villes
-		{
-			String TempVille = " ";
-			try
-			{
-				if (getParameter("xVille" + b)!=(null))
-				{
-					TempVille= getParameter("xVille"+b);
-					tabCoordVilleX[b] = Integer.parseInt(TempVille);
-				}
-
-
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-
-
-		for(int n = 0 ; n<tabCoordVilleY.length;++n) // boucle CoordY des villes
-		{
-			String TempVille= " ";
-			try
-			{
-				if (getParameter("yVille" + n)!=(null))
-				{
-					TempVille= getParameter("yVille"+n);
-					tabCoordVilleY[n] = Integer.parseInt(TempVille);
-				}
-
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-
-	}
-
-	public void start()
-	{
-		if( animation == null )
-		{
-			animation = new Thread( this );
-			animation.start();
-		}
-	}
-
-	public void stop()
-	{
-		if( animation != null )
-		{
-			animation = null;
-		}
-	}
-
-	public void paint( Graphics g )
-	{
-		// affichage des objets graphiques
-
-		//Dessiner Polygone
-		g.setColor(Color.CYAN);
-		g.fillRect(0,0,this.getSize().width,this.getSize().height);
+      //Dessiner Polygone
+      g.setColor(Color.CYAN);
+      g.fillRect(0, 0, maxX, maxY);
       g.setColor(Color.GREEN); // Remplir en quel couleur
-		g.drawPolygon(australie); // Dessiner polygon
-		g.fillPolygon(australie); // Remplir en vert
+      g.fillPolygon(australie); // Remplir en avec la dite couleur ^
+      g.drawPolygon(australie); // Dessiner polygon
+
 
       //Dessiner cercle sur coord des ville
-      for (int k = 0 ; k<5;++k) // ******Hardcodé a 5 mais a régler********* cuz toute les autre ville font un point a 0,0
+      for (int k = 0; k < NBVILLE; ++k)
       {
-         g.setColor(Color.BLACK);
-         g.drawOval(tabCoordVilleX[k],tabCoordVilleY[k],10,10); // Mesure pas exacte a essayer
-         g.fillOval(tabCoordVilleX[k],tabCoordVilleY[k],10,10);
+         if (tabCoordVilleX[k] != 0 || tabCoordVilleY[k] != 0 || tabNomVille[k] != null)
+         {
+            g.setColor(Color.BLACK);
+            g.drawOval(tabCoordVilleX[k], tabCoordVilleY[k], 10, 10); // Mesure pas exacte a essayer
+            g.fillOval(tabCoordVilleX[k], tabCoordVilleY[k], 10, 10);
+         }
       }
 
       //Affichage du String Australie
-      Font f = new Font("SansSerif",Font.BOLD,25);
-      g.setFont(f);
       g.setColor(Color.BLACK);
-      g.drawString("AUSTRALIE",0,420);
+      Font f = new Font("SansSerif", Font.BOLD, 25);
+      g.setFont(f);
+      g.drawString("AUSTRALIE", 0, 420);
 
       //Affichage du point qui va bouger
       g.setColor(Color.BLACK);
-      g.drawOval(posX,posY,15,15);
-      g.fillOval(posX,posY,15,15);
+      g.drawOval(posX, posY, GROSSEURHELICOPTERE, GROSSEURHELICOPTERE);
+      g.fillOval(posX, posY, GROSSEURHELICOPTERE, GROSSEURHELICOPTERE);
 
-		//Affichage des noms de villes
-		for(int j = 0;j<tabNomVille.length;++j)
-		{
-         Font t = new Font("SansSerif",Font.PLAIN,12);
-         g.setFont(t);
-			g.setColor(Color.BLACK);
-			g.drawString(tabNomVille[j],tabCoordVilleX[j]+20,tabCoordVilleY[j]+20); //A tester .. decaler de quelques Y pour ne pas écrire directement sur le point
-		}
-
-	}
+      //Affichage des noms de villes
+      for (int j = 0; j < tabNomVille.length; ++j)
+      {
+         if (tabCoordVilleX[j] != 0 || tabCoordVilleY[j] != 0 || tabNomVille[j] != null)
+         {
+            Font t = new Font("SansSerif", Font.PLAIN, 12);
+            g.setFont(t);
+            g.setColor(Color.BLACK);
+            g.drawString(tabNomVille[j], tabCoordVilleX[j] + 20, tabCoordVilleY[j] + 20); //A tester .. decaler de quelques Y pour ne pas écrire directement sur le point
+         }
+      }
+   }
 
    public void bougerPoint()
    {
-       boolean horsLimite = false;
-       final int DEPLACEMENT =5;
-       switch(direction)
-       {
+      final int DEPLACEMENT = 5;
+      switch (direction)
+      {
          case 0:
          {
-            if(posX+DEPLACEMENT < MAXX || posY-DEPLACEMENT < 0 )
-            {
-               posX +=DEPLACEMENT;
-               posY -=DEPLACEMENT;
-            }
-            else
-            {
-               horsLimite = true;
-            }
-
-            if(horsLimite)
-            {
-               direction = (int)Math.random()*4;
-            }
-            break;
+               if (posX + DEPLACEMENT < maxX - GROSSEURHELICOPTERE && posY - DEPLACEMENT > GROSSEURHELICOPTERE)
+               {
+                  posX += DEPLACEMENT;
+                  posY -= DEPLACEMENT;
+               }
+               else
+               {
+                  direction = (int)(Math.random() * 4);
+               }
+               break;
          }
          case 1:
          {
-            if(posX-DEPLACEMENT > 0 || posY+DEPLACEMENT < MAXY)
-            {
-               posX = posX-DEPLACEMENT;
-               posY = posY+DEPLACEMENT;
-            }
-            else
-            {
-               horsLimite = true;
-            }
-
-            if(horsLimite)
-            {
-               direction = (int)Math.random()*4;
-            }
-            break;
+               if (posX - DEPLACEMENT > GROSSEURHELICOPTERE && posY + DEPLACEMENT < maxY - GROSSEURHELICOPTERE)
+               {
+                  posX = posX - DEPLACEMENT;
+                  posY = posY + DEPLACEMENT;
+               }
+               else
+               {
+                  direction = (int)(Math.random() * 4);
+               }
+               break;
          }
          case 2:
          {
-            if(posX-DEPLACEMENT > 0 || posY-DEPLACEMENT < 0)
-            {
-                posX = posX-DEPLACEMENT;
-                posY = posY-DEPLACEMENT;
-            }
-            else
-            {
-               horsLimite = true;
-            }
-            if(horsLimite)
-            {
-               direction = (int)Math.random()*4;
-            }
-            break;
+               if (posX - DEPLACEMENT > GROSSEURHELICOPTERE && posY - DEPLACEMENT > GROSSEURHELICOPTERE)
+               {
+                  posX = posX - DEPLACEMENT;
+                  posY = posY - DEPLACEMENT;
+               }
+               else
+               {
+                  direction = (int)(Math.random() * 4);
+               }
+               break;
          }
          case 3:
          {
-            if(posX+DEPLACEMENT < MAXX || posY+DEPLACEMENT < MAXY)
-            {
-               posX = posX+DEPLACEMENT;
-               posY = posY+DEPLACEMENT;
-            }
-            else
-            {
-               horsLimite = true;
-            }
-            if(horsLimite)
-            {
-               direction = (int)Math.random()*4;
-            }
-            break;
+               if (posX + DEPLACEMENT < maxX - GROSSEURHELICOPTERE && posY + DEPLACEMENT < maxY - GROSSEURHELICOPTERE)
+               {
+                  posX = posX + DEPLACEMENT;
+                  posY = posY + DEPLACEMENT;
+               }
+               else
+               {
+                  direction = (int)(Math.random() * 4);
+               }
+               break;
          }
-       }
+      }
    }
 
-	public void run()
-	{
-		boolean pasFini = true;
-		while( pasFini )//boucle infinie
-		{
-			try
-			{
+   public void run()
+   {
+      boolean pasFini = true;
+      while (pasFini)//boucle infinie
+      {
+         try
+         {
             bougerPoint();
-				Thread.sleep( 500 ); //delai 3 secondes...a checker
-				repaint();
-			}
-			catch( InterruptedException ie )
-			{
-				// ne devrait jamais arriver
-			}
-		}
-	}
+            Thread.sleep(100); //delai 3 secondes...a checker
+            repaint();
+         }
+         catch (InterruptedException ie)
+         {
+            // ne devrait jamais arriver
+         }
+      }
+   }
 }
